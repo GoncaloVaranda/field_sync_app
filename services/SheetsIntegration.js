46fc24617691410b58b64cb9323ed0ae2bf43943
FranciscoParreira65620 <f.parreira@campus.fct.unl.pt>
2025-07-18 18:04:34 +0100

    Corrigido o SheetsIntegration.js

---
+++ services/SheetsIntegration.js
@@ -0,0 +1,34 @@
+const BASE_URL = "https://fieldsync-adc.oa.r.appspot.com/rest"; // ajusta para a tua URL
+
+export default class WorksheetService {
+    static async importWorksheet(token, worksheetData) {
+        const url = `${BASE_URL}/worksheets/worksheet-import`;
+        const payload = {
+            worksheetData
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(payload),
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                throw new Error(`Erro no servidor (${response.status}): ${errorText}`);
+            }
+
+            return await response.json();
+
+        } catch (err) {
+            console.error('Falha na requisição de importação:', err);
+            throw err;
+        }
+    };
+
+
+}
