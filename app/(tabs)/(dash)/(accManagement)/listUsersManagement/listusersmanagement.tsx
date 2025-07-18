3604db490937a84b23bdb533ce4ec3398b0602af
GoncaloVaranda <g.varanda@campus.fct.unl.pt>
2025-07-18 17:10:31 +0100

    re add no listprivate

--- app/(tabs)/(dash)/(accManagement)/listUsersManagement/listusersmanagement.tsx
+++ app/(tabs)/(dash)/(accManagement)/listUsersManagement/listusersmanagement.tsx
@@ -32,6 +32,15 @@ export default function Listusersmanagement() {
                         <Text style={styles.buttonText}>Listar utilizadores</Text>
                     </Pressable>
 
+                    {(role === "Root" || role === "System Back-Office" ) &&(
+                    <Pressable
+                        style={styles.button}
+                        onPress={() => router.push(`/listUsersManagement/listusersprivateaccounts?token=${token}&username=${username}&role=${role}`)}
+                    >
+                        <Text style={styles.buttonText}>Listar contas privadas</Text>
+                    </Pressable>
+                    )}
+                    
 
                     {(role === "Root" || role === "System Back-Office" ) &&(
                     <Pressable
